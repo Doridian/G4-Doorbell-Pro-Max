497da84e093ba811190bd4c7c4c0a49e4856056a
Doridian <git@doridian.net>
2023-05-28 16:12:21 -0700

    dos2unix

--- docker/build.sh
+++ docker/build.sh
@@ -7,7 +7,7 @@ if [ "$(uname -m)" = "aarch64" ]; then
 fi
 
 runcc() {
-    $CC -nodefaultlibs -L/fw/image/rootfs/lib "$@" /fw/image/rootfs/lib/libc.so.6 /fw/image/rootfs/lib/ld-2.23.so -fno-stack-protector
+    $CC -O3 -nodefaultlibs -L/fw/image/rootfs/lib "$@" /fw/image/rootfs/lib/libc.so.6 /fw/image/rootfs/lib/ld-2.23.so -fno-stack-protector
 }
 
 copycam() {
@@ -15,5 +15,8 @@ copycam() {
     cat "$1" | ssh ubnt@camera-front-door.foxden.network "rm -f '/var/$1' && echo Loading... && cat /dev/stdin > '/var/$1' && echo CHMod && chmod 755 '/var/$1'"
 }
 
-runcc ./mynfc.c -o mynfc -lnxp-nfc
-runcc ./myfp.c -o myfp -lbmkt -lpthread-2.23
+mkdir -p ./dist/bin ./dist/lib
+
+cp /fw/image/rootfs/lib/libnxp-nfc.so ./dist/lib
+runcc mynfc.c -o ./dist/bin/mynfc -lnxp-nfc
+runcc myfp.c -o ./dist/bin/myfp -lbmkt -lpthread-2.23
