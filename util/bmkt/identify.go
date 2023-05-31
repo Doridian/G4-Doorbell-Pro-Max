416dde64532074ff0422c48b79f5a65b72f95b12
Doridian <git@doridian.net>
2023-05-30 17:47:22 -0700

    fix finger

--- util/bmkt/identify.go
+++ util/bmkt/identify.go
@@ -9,19 +9,19 @@ func (ctx *BMKTContext) autoIdentify() {
 		return
 	}
 
-	_, err := ctx.identifyNoCancel()
+	user, finger, err := ctx.identifyNoCancel()
 	if err != nil {
 		ctx.logger.Warn().Bool("success", false).Str("op", "auto_identify").Err(err).Send()
 		return
 	}
-	ctx.logger.Info().Bool("success", true).Str("op", "auto_identify").Send()
+	ctx.logger.Info().Bool("success", true).Str("op", "auto_identify").Str("user", user).Int("finger", finger).Send()
 }
 
-func (ctx *BMKTContext) identifyNoCancel() (string, error) {
+func (ctx *BMKTContext) identifyNoCancel() (string, int, error) {
 	ctx.sessionLock.Lock()
 	defer ctx.sessionLock.Unlock()
 	if ctx.state != IF_STATE_IDLE {
-		return "", errors.New("interrupted by other operation")
+		return "", 0, errors.New("interrupted by other operation")
 	}
 
 	ctx.state = IF_STATE_IDENTIFYING
@@ -32,21 +32,21 @@ func (ctx *BMKTContext) identifyNoCancel() (string, error) {
 		return C.bmkt_identify(ctx.session)
 	})
 	if err != nil {
-		return "", err
+		return "", 0, err
 	}
 	ctx.waitForIdle()
 
 	if ctx.lastIdentifyResult == C.BMKT_SUCCESS && ctx.IdentifyCallback != nil {
-		go ctx.IdentifyCallback(ctx.lastIdentifyUser, ctx.lastVerifyFinger)
+		go ctx.IdentifyCallback(ctx.lastIdentifyUser, ctx.lastIdentifyFinger)
 	}
 
-	return ctx.lastIdentifyUser, wrapBMKTError(ctx.lastIdentifyResult)
+	return ctx.lastIdentifyUser, ctx.lastIdentifyFinger, wrapBMKTError(ctx.lastIdentifyResult)
 }
 
-func (ctx *BMKTContext) Identify() (string, error) {
+func (ctx *BMKTContext) Identify() (string, int, error) {
 	err := ctx.Cancel()
 	if err != nil {
-		return "", err
+		return "", 0, err
 	}
 
 	return ctx.identifyNoCancel()
